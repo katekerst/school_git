219e2ad36fbc6ca9a22552aa6455e799017b8a90
Sigrin Olive <siolive@mi-s3.21-school.ru>
2019-04-30 12:08:13 +0300

    gnl before check

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: siolive <siolive@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/21 11:06:00 by siolive           #+#    #+#             */
-/*   Updated: 2019/04/27 14:56:42 by siolive          ###   ########.fr       */
+/*   Updated: 2019/04/30 10:56:19 by siolive          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,37 +19,36 @@ t_list				*ft_getlist(t_list **file, int fd)
 	if (!file)
 		return (NULL);
 	new = *file;
-	while (new != NULL)
+	while (new)
 	{
 		if ((int)new->content_size == fd)
 			return (new);
 		new = new->next;
 	}
-	new = ft_lstnew("", fd);
-	ft_lstadd(file, new);
+	ft_lstadd(file, new = ft_lstnew("", fd));
 	return (new);
 }
 
 int					ft_read(int fd, char **copy)
 {
 	char			buffer[BUFF_SIZE + 1];
-	char			*temp;
 	int				ret;
+	char			*temp;
 
-	while ((ret = read(fd, buffer, BUFF_SIZE)) > 0)
+	while ((ret = read(fd, buffer, BUFF_SIZE)))
 	{
 		buffer[ret] = '\0';
 		temp = *copy;
-		if (!(*copy = ft_strjoin(*copy, buffer)))
+		if (!(*copy = ft_strjoin(temp, buffer)))
 			return (-1);
 		free(temp);
-		if (ft_strchr(buffer, '\n') || ft_strchr(buffer, -1))
+		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
 	return (ret);
 }
 
-int					ft_strccpy(char **line, char *text, char symb1, char symb2)
+int					ft_strccpy(char **line, char *text, char symb)
 {
 	int				i;
 	int				j;
@@ -59,9 +58,10 @@ int					ft_strccpy(char **line, char *text, char symb1, char symb2)
 	i = 0;
 	j = 0;
 	temp = *line;
-	while (text[i] && text[i] != symb1 && text[i] != symb2)
+	while (text[i] && text[i] != symb)
 		i++;
-	if (!(new = ft_strnew(i)))
+	new = ft_strnew(i);
+	if (!new)
 		return (0);
 	while (text[j] && j < i)
 	{
@@ -74,27 +74,27 @@ int					ft_strccpy(char **line, char *text, char symb1, char symb2)
 
 int					get_next_line(const int fd, char **line)
 {
-	char			buffer[BUFF_SIZE + 1];
+	char			buf[BUFF_SIZE + 1];
 	int				ret;
 	static t_list	*file;
 	t_list			*node;
 	char			*temp;
 
-	if (!line || fd < 0 || BUFF_SIZE < 0 || read(fd, buffer, 0) < 0)
-		return (-1);
-	if ((node = ft_getlist(&file, fd)) == NULL)
+	if (!line || fd < 0 || fd > 10240 ||
+			read(fd, buf, 0) < 0 || !(node = ft_getlist(&file, fd)))
 		return (-1);
 	temp = (char *)node->content;
-	ret = ft_read(fd, &temp);
+	if ((ret = ft_read(fd, &temp) == -1))
+		return (-1);
 	node->content = temp;
-	if (ret == 0 && *temp == 0)
+	if (!ret && !*temp)
 		return (0);
-	ret = ft_strccpy(line, (char *)node->content, '\n', -1);
+	ret = ft_strccpy(line, (char *)node->content, '\n');
 	temp = (char *)node->content;
-	if (temp[ret] == '\0')
+	if (!temp[ret])
 		ft_strclr(node->content);
 	else
-		node->content = ft_strdup(&(node->content[ret + 1]));
+		node->content = ft_strdup(&temp[ret + 1]);
 	free(temp);
 	return (1);
 }
